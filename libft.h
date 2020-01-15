3a0e37447bb59a2db2e15435c2c360ae0904fe8b
Estecka <estecka@galacsys.com>
2020-01-15 15:48:43 +0100

    Added gnl to the header.

--- libft.h
+++ libft.h
@@ -89,4 +89,11 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void*));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*));
 
+/*
+** get_next_line
+*/
+
+int		get_next_char(int fd, char *dst);
+int		get_next_line(int fd, char **line);
+
 #endif
